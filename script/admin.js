49461bcc0743698ad6d186fa85856d516a33ca07
SiphoChris <siphonkebe@gmail.com>
2024-06-18 13:00:41 +0200

    footer layout in html, get the current year in js functionality, and a few optimizations on styling

---
+++ script/admin.js
@@ -0,0 +1,13 @@
+function displayNav() {
+    var x = document.getElementById("nav-bar");
+    if (x.className === "nav-bar") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-bar";
+    }
+  }
+
+
+
+document.querySelector("[current-year]").textContent =
+        new Date().getUTCFullYear();
