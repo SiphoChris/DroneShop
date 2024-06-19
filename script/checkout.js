526b3ff48bb9f57fe673bffbb9d6136005be438c
SiphoChris <siphonkebe@gmail.com>
2024-06-19 08:39:36 +0200

    basic functionality for the checkout page

--- script/checkout.js
+++ script/checkout.js
@@ -1,12 +1,27 @@
-function displayNav() {
-    var x = document.getElementById("nav-bar");
-    if (x.className === "nav-bar") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav-bar";
-    }
-  }
+let checkoutContainer = document.querySelector('[checkout-container]')
 
+
+function displayNav() {
+  var x = document.getElementById("nav-bar");
+  if (x.className === "nav-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-bar";
+  }
+}
 
 document.querySelector("[current-year]").textContent =
-        new Date().getUTCFullYear();
+  new Date().getUTCFullYear();
+
+
+  let checkoutItems = localStorage.getItem('checkout')
+
+  function displayCheckoutItems(){
+    checkoutItems.forEach(item => {
+      checkoutContainer.innerHTML += `
+        <div>
+          <p>${item.productName}</p>
+        </div>
+      `
+    })
+  }
